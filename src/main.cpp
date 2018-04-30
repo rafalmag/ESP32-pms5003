f4244595dcd21b4d178e0fc6a14cf91845ce9a05
Rafal Magda <rafalmag@gmail.com>
2018-04-30 22:11:02 +0200

    with 'set' connected to 5 pin

--- src/main.cpp
+++ src/main.cpp
@@ -6,34 +6,48 @@ HardwareSerial Serial1(2); // 16,17
 
 PMS pms(Serial1);
 PMS::DATA data;
+int setPin = 5;
 
 void setup()
 {
-  Serial.begin(9600);   // 1,3 - connected via usb
-  delay(100);
-  Serial.println("Hellloooooo");
-  Serial.println(RX);
-  Serial.println(TX);
-  Serial.println(SDA);
-  Serial.println(SCL);
-  Serial1.begin(9600);  // GPIO2 (D4 pin on ESP-12E Development Board)
+    Serial.begin(9600);  // 1,3 - connected via usb
+    Serial1.begin(9600); // GPIO2 (D4 pin on ESP-12E Development Board)
+
+    digitalWrite(setPin, HIGH);
+    pinMode(setPin, OUTPUT);
+    pms.passiveMode();
 }
 
 void loop()
 {
-  if (pms.read(data))
-  {
-    Serial.println("Data:");
+    Serial.println("Wake up, wait 20 seconds for stable readings...");
+    digitalWrite(setPin, HIGH);
+    pms.wakeUp();
+    delay(20000);
 
-    Serial.print("PM 1.0 (ug/m3): ");
-    Serial.println(data.PM_AE_UG_1_0);
+    Serial.println("Send request read...");
+    pms.requestRead();
 
-    Serial.print("PM 2.5 (ug/m3): ");
-    Serial.println(data.PM_AE_UG_2_5);
+    bool status = pms.read(data, 10000);
+    if (status)
+    {
+        Serial.print("PM 1.0 (ug/m3): ");
+        Serial.println(data.PM_AE_UG_1_0);
 
-    Serial.print("PM 10.0 (ug/m3): ");
-    Serial.println(data.PM_AE_UG_10_0);
+        Serial.print("PM 2.5 (ug/m3): ");
+        Serial.println(data.PM_AE_UG_2_5);
 
-    Serial.println();
-  }
+        Serial.print("PM 10.0 (ug/m3): ");
+        Serial.println(data.PM_AE_UG_10_0);
+
+        Serial.println();
+    }
+    else
+    {
+        Serial.println("No data.");
+    }
+    Serial.println("Going to sleep for 10 seconds.");
+    pms.sleep();
+    digitalWrite(setPin, LOW);
+    delay(10000);
 }
