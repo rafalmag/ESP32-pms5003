f967aee3499d5b61b1bd09c3b25ffc04665fec09
Rafal Magda <rafalmag@gmail.com>
2018-05-02 01:30:44 +0200

    average from 5 reads minus min and max

--- src/main.cpp
+++ src/main.cpp
@@ -75,6 +75,72 @@ void setup()
     Serial.println(WiFi.localIP());
 }
 
+struct pms5003
+{
+    float pm1, pm2, pm10;
+};
+
+pms5003 pm;
+int readIter = 5;
+bool updatePmReads()
+{
+    int reads = 0;
+    uint16_t min1 = 0, max1 = 0, sum1 = 0,
+             min2 = 0, max2 = 0, sum2 = 0,
+             min10 = 0, max10 = 0, sum10 = 0;
+    for (int i = 0; i < readIter; i++)
+    {
+        pms.requestRead();
+        if (pms.read(data, 10000))
+        {
+            if (reads == 0)
+            {
+                min1 = data.PM_AE_UG_1_0;
+                max1 = data.PM_AE_UG_1_0;
+                sum1 = data.PM_AE_UG_1_0;
+                min2 = data.PM_AE_UG_2_5;
+                max2 = data.PM_AE_UG_2_5;
+                sum2 = data.PM_AE_UG_2_5;
+                min10 = data.PM_AE_UG_10_0;
+                max10 = data.PM_AE_UG_10_0;
+                sum10 = data.PM_AE_UG_10_0;
+            }
+            else
+            {
+                if (data.PM_AE_UG_1_0 < min1)
+                    min1 = data.PM_AE_UG_1_0;
+                if (max1 < data.PM_AE_UG_1_0)
+                    max1 = data.PM_AE_UG_1_0;
+                sum1 += data.PM_AE_UG_1_0;
+                if (data.PM_AE_UG_2_5 < min2)
+                    min2 = data.PM_AE_UG_2_5;
+                if (max2 < data.PM_AE_UG_2_5)
+                    max2 = data.PM_AE_UG_2_5;
+                sum2 += data.PM_AE_UG_2_5;
+                if (data.PM_AE_UG_10_0 < min10)
+                    min10 = data.PM_AE_UG_1_0;
+                if (max10 < data.PM_AE_UG_10_0)
+                    max10 = data.PM_AE_UG_10_0;
+                sum10 += data.PM_AE_UG_10_0;
+            }
+            reads++;
+        }
+    }
+    if (reads > 2)
+    {
+        pm.pm1 = (float)(sum1 - min1 - max1) / (float)(reads - 2);
+        pm.pm2 = (float)(sum2 - min2 - max2) / (float)(reads - 2);
+        pm.pm10 = (float)(sum10 - min10 - max10) / (float)(reads - 2);
+    }
+    else
+    {
+        pm.pm1 = min1;
+        pm.pm2 = min2;
+        pm.pm10 = min10;
+    }
+    return reads > 0;
+}
+
 void loop()
 {
     Serial.println("Wake up, wait 30 seconds for stable readings...");
@@ -87,19 +153,18 @@ void loop()
     https.addHeader("Content-Type", "application/json; charset=utf-8");
 
     Serial.println("Send request read...");
-    pms.requestRead();
-    bool status = pms.read(data, 10000);
+    bool status = updatePmReads();
     if (status)
     {
         Serial.print("PM 1.0 (ug/m3): ");
-        Serial.println(data.PM_AE_UG_1_0);
+        Serial.println(pm.pm1);
         Serial.print("PM 2.5 (ug/m3): ");
-        Serial.println(data.PM_AE_UG_2_5);
+        Serial.println(pm.pm2);
         Serial.print("PM 10.0 (ug/m3): ");
-        Serial.println(data.PM_AE_UG_10_0);
+        Serial.println(pm.pm10);
         Serial.println();
 
-        String req = String("{\"value1\":") + data.PM_AE_UG_1_0 + ",\"value2\":" + data.PM_AE_UG_2_5 + ",\"value3\":" + data.PM_AE_UG_10_0 + "}";
+        String req = String("{\"value1\":") + pm.pm1 + ",\"value2\":" + pm.pm2 + ",\"value3\":" + pm.pm10 + "}";
         int httpCode = https.POST(req);
 
         // httpCode will be negative on error
